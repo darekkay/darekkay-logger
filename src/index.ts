8a804cf791779017485e6ac612a3f2f5f895060a
Darek Kay <hello@darekkay.com>
2021-04-26 21:14:43 +0200

    Remove kleur dependency

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import logLevel, { LogLevelDesc } from "loglevel";
 import symbols from "log-symbols";
-import { red, blue, yellow } from "kleur";
 
 type LOG_LEVEL = "trace" | "debug" | "info" | "warn" | "error" | "silent";
 
@@ -15,12 +14,12 @@ logLevel.setDefaultLevel(level[process.env.NODE_ENV ?? "development"]);
 const logger = {
   trace: (...args: any[]) => logLevel.trace(...args),
   debug: (...args: any[]) => logLevel.debug(" ", ...args),
-  info: (...args: any[]) => logLevel.info(blue(symbols.info), ...args),
-  warn: (...args: any[]) => logLevel.warn(yellow(symbols.warning), ...args),
-  error: (...args: any[]) => logLevel.error(red(symbols.error), ...args),
+  info: (...args: any[]) => logLevel.info(symbols.info, ...args),
+  warn: (...args: any[]) => logLevel.warn(symbols.warning, ...args),
+  error: (...args: any[]) => logLevel.error(symbols.error, ...args),
 
   log: (...args: any[]) => logLevel.log(" ", ...args),
-  success: (...args: any[]) => logLevel.info(red(symbols.success), ...args),
+  success: (...args: any[]) => logLevel.info(symbols.success, ...args),
 
   setLevel: (levelDescription: LogLevelDesc, persist?: boolean) =>
     logLevel.setLevel(levelDescription, persist),
